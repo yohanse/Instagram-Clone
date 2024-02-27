ed61f833011a965301ce55ec4d2bdaa9efdd7ad3
yohanse <mehabawyohanse793@gmail.com>
2024-02-27 22:29:05 +0300

    implements local data source and rmote data source

--- mobile/lib/features/auth/data/datasources/local_data_source.dart
+++ mobile/lib/features/auth/data/datasources/local_data_source.dart
@@ -1,6 +1,23 @@
+import 'package:mobile/core/error/exception.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class AuthLocalDataSource {
   SharedPreferences get prefs;
-  Future<void> cacheToken({String token});
+  Future<bool> cacheToken({required String token});
+}
+
+class AuthLocalDataSourceImpl implements AuthLocalDataSource {
+  @override
+  Future<bool> cacheToken({required String token}) async {
+    try {
+      await prefs.setString('token', token);
+      return true;
+    } catch (e) {
+      throw CacheException();
+    }
+  }
+
+  @override
+  SharedPreferences prefs;
+  AuthLocalDataSourceImpl({required this.prefs});
 }
--- mobile/lib/features/auth/data/datasources/remote_data_source.dart
+++ mobile/lib/features/auth/data/datasources/remote_data_source.dart
@@ -1,6 +1,17 @@
 import '../models/auth_model.dart';
 
 abstract class AuthRemoteDataSource {
-  Future<AuthModel> checkConfirmation({String email, String confimationCode});
-  Future<AuthModel> checkEmail({String email});
+  Future<AuthModel> checkConfirmation({ required String email, required String confimationCode});
+  Future<AuthModel> checkEmail({required String email});
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource{
+  @override
+  Future<AuthModel> checkConfirmation({required String email, required String confimationCode}) {
+  }
+
+  @override
+  Future<AuthModel> checkEmail({required String email}) {
+  }
+
 }
