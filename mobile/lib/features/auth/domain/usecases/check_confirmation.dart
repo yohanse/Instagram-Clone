4ed8a2898aac3ea01d0edbae945388340f2cee8f
yohanse <mehabawyohanse793@gmail.com>
2024-02-27 20:29:36 +0300

    use a usecase class to make the the usecase class callable

--- mobile/lib/features/auth/domain/usecases/check_confirmation.dart
+++ mobile/lib/features/auth/domain/usecases/check_confirmation.dart
@@ -1,12 +1,26 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:mobile/core/error/failures.dart';
+import 'package:mobile/core/usecase/usecase.dart';
 import 'package:mobile/features/auth/domain/entities/auth_entitie.dart';
 import 'package:mobile/features/auth/domain/repository/auth_repository.dart';
 
-class CheckConfirmation {
+class CheckConfirmation implements Usecase<AuthEntitie, Params> {
   final AuthRepository authRepository;
   CheckConfirmation({required this.authRepository});
-  Future<Either<Failure, AuthEntitie>> call({required String email, required String confimationCode}) async {
-    return authRepository.checkConfirmation(email: email, confimationCode: confimationCode);
+
+  @override
+  Future<Either<Failure, AuthEntitie>> call(Params params) async {
+    return authRepository.checkConfirmation(
+        email: params.email, confimationCode: params.confimationCode);
   }
+}
+
+class Params extends Equatable {
+  final String email, confimationCode;
+
+  const Params({required this.email, required this.confimationCode}):super();
+  
+  @override
+  List<Object?> get props => [email, confimationCode];
 }
--- mobile/lib/features/auth/domain/usecases/check_email_auth.dart
+++ mobile/lib/features/auth/domain/usecases/check_email_auth.dart
@@ -1,12 +1,26 @@
 import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
 import 'package:mobile/core/error/failures.dart';
+import 'package:mobile/core/usecase/usecase.dart';
 import 'package:mobile/features/auth/domain/entities/auth_entitie.dart';
 import 'package:mobile/features/auth/domain/repository/auth_repository.dart';
 
-class CheckEmail {
+class CheckEmail implements Usecase<AuthEntitie, Params> {
   final AuthRepository authRepository;
   CheckEmail({required this.authRepository});
-  Future<Either<Failure, AuthEntitie>> call({required String email}) async {
-    return authRepository.checkEmail(email: email);
+
+  @override
+  Future<Either<Failure, AuthEntitie>> call(Params params) async {
+    return authRepository.checkEmail(
+        email: params.email);
   }
+}
+
+class Params extends Equatable {
+  final String email;
+
+  const Params({required this.email}):super();
+  
+  @override
+  List<Object?> get props => [email];
 }
