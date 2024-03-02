75b85c9964094318971e65ecbd9fddd4273533fb
yohanse <mehabawyohanse793@gmail.com>
2024-03-02 15:07:29 +0300

    adding corse

--- Backend/InstagramClone/settings.py
+++ Backend/InstagramClone/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -154,3 +156,5 @@ EMAIL_HOST_PASSWORD = "tzui cppu uwsn ysoa"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+CORS_ORIGIN_ALLOW_ALL = True
