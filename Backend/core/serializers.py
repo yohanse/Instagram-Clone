177d6d47cd6786c82c5f87e41738558bf1f18b9e
yohanse <mehabawyohanse793@gmail.com>
2024-03-17 16:49:23 +0300

    finishe register

--- Backend/core/serializers.py
+++ Backend/core/serializers.py
@@ -5,8 +5,17 @@ from . import models
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         model = models.User
-        fields = ['id', 'email']
 
+    def perform_create(self, validated_data):
+        user = super().perform_create(validated_data)
+        email = user.email
+
+        try:
+            confirmation = models.Confirmation.objects.get(email=email, is_confirmed=True)
+        except models.Confirmation.DoesNotExist:
+            raise serializers.ValidationError({'error': 'Email not confirmed. Please confirm your email to proceed.'})
+
+        return user
 
 class ConfirmationSerializer(serializers.ModelSerializer):
     class Meta:
