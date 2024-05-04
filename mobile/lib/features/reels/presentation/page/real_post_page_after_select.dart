2b60a582b92eaf36a0f66acbdcff8902bfc14936
yohanse <mehabawyohanse793@gmail.com>
2024-05-04 12:23:24 +0300

    finishe the ui

--- mobile/lib/features/reels/presentation/page/real_post_page_after_select.dart
+++ mobile/lib/features/reels/presentation/page/real_post_page_after_select.dart
@@ -21,103 +21,124 @@ class ReelPostPageAfterSelect extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocConsumer<AddReelBloc, AddReelState>(
       builder: (context, state) {
-        return Scaffold(
-          backgroundColor: Colors.black,
-          appBar: AppBar(
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back_ios_new_rounded),
-              color: Colors.white,
-              onPressed: () {
-                context.go("/");
-              },
-            ),
-            title: Text(
-              "New Reel",
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            backgroundColor: Colors.black,
-          ),
-          body: Column(
-            children: [
-              Expanded(
-                flex: 1,
-                child: VideoApp(
-                  path: path,
+        return Stack(
+          children: [
+            Scaffold(
+              backgroundColor: Colors.black,
+              appBar: AppBar(
+                leading: IconButton(
+                  icon: Icon(Icons.arrow_back_ios_new_rounded),
+                  color: Colors.white,
+                  onPressed: () {
+                    context.go("/");
+                  },
                 ),
+                title: Text(
+                  "New Reel",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                backgroundColor: Colors.black,
               ),
-              Expanded(
-                flex: 1,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      TextField(
-                        controller: _tagController,
-                        decoration: InputDecoration(
-                          hintText: "Write a caption ...",
-                          hintStyle:
-                              TextStyle(color: Colors.grey, fontSize: 14),
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide.none,
+              body: Column(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: VideoApp(
+                      path: path,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          TextField(
+                            controller: _tagController,
+                            decoration: InputDecoration(
+                              hintText: "Write a caption ...",
+                              hintStyle:
+                                  TextStyle(color: Colors.grey, fontSize: 14),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide.none,
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                            style: TextStyle(color: Colors.white),
                           ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide.none,
+                          SizedBox(
+                            height: 3,
                           ),
-                        ),
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      SizedBox(
-                        height: 3,
-                      ),
-                      Container(
-                        color: Colors.grey,
-                        width: double.infinity,
-                        height: 1,
-                      ),
-                      SizedBox(
-                        height: 8,
-                      ),
-                      Center(
-                        child: SizedBox(
-                          height: 50,
-                          width: 120,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              BlocProvider.of<AddReelBloc>(context).add(
-                                AddReelClick(
-                                    content: _tagController.text,
-                                    filePath: path.path),
-                              );
-                            },
-                            style: ButtonStyle(
-                              backgroundColor:
-                                  MaterialStateProperty.all<Color>(Colors.blue),
-                              shape: MaterialStateProperty.all<
-                                  RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10.0),
+                          Container(
+                            color: Colors.grey,
+                            width: double.infinity,
+                            height: 1,
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Center(
+                            child: SizedBox(
+                              height: 50,
+                              width: 120,
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  BlocProvider.of<AddReelBloc>(context).add(
+                                    AddReelClick(
+                                        content: _tagController.text,
+                                        filePath: path.path),
+                                  );
+                                },
+                                style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.blue),
+                                  shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                  ),
+                                ),
+                                child: Text(
+                                  "Next",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 19,
+                                    fontWeight: FontWeight.w600,
+                                  ),
                                 ),
                               ),
                             ),
-                            child: Text(
-                              "Next",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 19,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            if (state is AddReelLoading)
+              Container(
+                height: double.maxFinite,
+                width: double.maxFinite,
+                color: Colors.black.withOpacity(0.5),
+                child: Center(
+                child: Container(
+                  height: 60,
+                  width: 60,
+                  child: CircularProgressIndicator(
+                    color: Colors.white,
+                    strokeWidth: 2,
                   ),
                 ),
               ),
-            ],
-          ),
+              ),
+          ],
         );
       },
       listener: (context, state) {
