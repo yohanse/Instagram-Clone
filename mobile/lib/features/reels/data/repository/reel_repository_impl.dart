d7c048556b436b994fd3ac1762f64710f020af57
yohanse <mehabawyohanse793@gmail.com>
2024-05-04 11:10:29 +0300

    finishe implementing repository of add reel

--- mobile/lib/features/reels/data/repository/reel_repository_impl.dart
+++ mobile/lib/features/reels/data/repository/reel_repository_impl.dart
@@ -97,4 +97,21 @@ class ReelRepositoryImpl implements ReelRepository {
     }
   }
   
+  @override
+  Future<Either<Failure, ReelEntite>> addReel(String filePath, String content) async{
+    if (await networkInfo.isConnected) {
+      
+      try {
+        final ans = await reelRemoteDataSource.addReel(filePath, content);
+        return Right(ans);
+      } on ServerException {
+        return const Left(ServerFailure("Server not working properly."));
+      } on EmailNotCorrectException {
+        return const Left(EmailNotCorrectFailure("Email not correct"));
+      }
+    } else {
+      return const Left(NetworkFailure("Netwrok error."));
+    }
+  }
+  
 }
