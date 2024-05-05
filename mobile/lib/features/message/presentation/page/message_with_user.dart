8123412ca14c4d5cf4b0a1734d3ef08f9dab86a3
yohanse <mehabawyohanse793@gmail.com>
2024-05-05 14:26:20 +0300

    fix all the ui and start websocket

--- mobile/lib/features/message/presentation/page/message_with_user.dart
+++ mobile/lib/features/message/presentation/page/message_with_user.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:go_router/go_router.dart';
 import 'package:mobile/features/profile/Domain/Entite/profile_entitie.dart';
 
 class ChatPage extends StatelessWidget {
@@ -11,6 +13,14 @@ class ChatPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () => context.go("/"),
+        ),
         title: ListTile(
           leading: CircleAvatar(
             backgroundImage: NetworkImage(userParams.profile_image),
@@ -25,38 +35,63 @@ class ChatPage extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: Text(
-          "Chat Page",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-          ),
-        ),
-      ),
-      bottomNavigationBar: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Column(
         children: [
-          TextField(
-            controller: messageController,
-            decoration: InputDecoration(
-              disabledBorder: InputBorder.none,
-              suffix: IconButton(
-                icon: Icon(
-                  Icons.send,
-                  color: Colors.blue,
+          Expanded(
+            child: Center(
+              child: Text(
+                "Chat Page",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
                 ),
-                onPressed: () {},
               ),
-              hintText: "Message...",
-              hintStyle: TextStyle(
-                color: Colors.grey,
-              ),
-              border: InputBorder.none,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              
+              children: [
+                TextField(
+                  controller: messageController,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                  ),
+                  decoration: InputDecoration(
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        Icons.send,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {},
+                    ),
+                    disabledBorder: InputBorder.none,
+                    hintText: "Message...",
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                Container(
+                  color: Colors.grey,
+                  width: double.maxFinite,
+                  height: 1,
+                ),
+                SizedBox(height: 8,),
+              ],
             ),
           ),
         ],
       ),
+      // Container(
+      //   height: double.maxFinite,
+      //   width: double.maxFinite,
+      //   child:
+
+      // ),
     );
   }
 }
