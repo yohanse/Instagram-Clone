07c97616615ceca60e0ebfcec5537dcaf10ab72f
yohanse <mehabawyohanse793@gmail.com>
2024-05-05 11:46:06 +0300

    add get user usecase

---
+++ mobile/lib/features/message/Domain/usecase/get_users_usecase.dart
@@ -0,0 +1,25 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:mobile/core/error/failures.dart';
+import 'package:mobile/core/usecase/usecase.dart';
+import 'package:mobile/features/message/Domain/repository/message_repository.dart';
+import 'package:mobile/features/profile/Domain/Entite/profile_entitie.dart';
+import 'package:mobile/features/reels/domain/Entitie/reel_entitie.dart';
+import 'package:mobile/features/reels/domain/repository/reel_repository.dart';
+
+
+
+class GetUserUsecase implements Usecase<List<ProfileEntitie>, ParamsGetUser> {
+  final MessageRepository messageRepository;
+  GetUserUsecase({required this.messageRepository});
+
+  @override
+  Future<Either<Failure, List<ProfileEntitie>>> call(ParamsGetUser params) async {
+    return await messageRepository.getUsers();
+  }
+}
+
+class ParamsGetUser extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
