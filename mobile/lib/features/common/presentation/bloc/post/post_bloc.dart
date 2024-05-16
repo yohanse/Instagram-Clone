c176e7b93af27792c042e1b2a67a5968f872b7fc
yohanse <mehabawyohanse793@gmail.com>
2024-05-16 11:36:01 +0300

    add on event on post bloc

--- mobile/lib/features/common/presentation/bloc/post/post_bloc.dart
+++ mobile/lib/features/common/presentation/bloc/post/post_bloc.dart
@@ -2,20 +2,65 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:mobile/features/common/post/domain/entities/post_entitie.dart';
 import 'package:mobile/features/common/post/domain/usecases/get_all_post.dart';
+import 'package:mobile/features/reels/domain/Entitie/reel_entitie.dart';
+
+import '../../../post/domain/usecases/like_post_usecase.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
 
 class PostBloc extends Bloc<PostEvent, PostState> {
   final GetAllPostUseCase getAllPostUseCase;
-  PostBloc({required this.getAllPostUseCase}) : super(PostInitial()) {
+  final LikePostUseCase likePostUseCase;
+  PostBloc({required this.getAllPostUseCase, required this.likePostUseCase})
+      : super(PostInitial()) {
     on<GetAllPostsEvent>((event, emit) async {
       emit(PostLoading());
-      
+
       var posts = await getAllPostUseCase(ParamsGetAllPost());
-      
+
       posts.fold((l) => emit(PostError(message: l.message)),
           (r) => emit(PostLoaded(post: r)));
+    });
+
+    on<UnLikePostEvent>((event, emit) async {
+      var posts = await getAllPostUseCase(ParamsGetAllPost());
+
+      posts.fold((l) => emit(PostError(message: l.message)),
+          (r) => emit(PostLoaded(post: r)));
+    });
+
+    on<LikePostEvent>((event, emit) async {
+      
+
+      var posts = await likePostUseCase(ParamsLikePost(postId: event.postId));
+
+      posts.fold((l) {}, (r) {
+        if (state is PostLoaded) {
+          List<PostEntite> post = [];
+          for (int i = 0; i < (state as PostLoaded).post.length; i++) {
+            if ((state as PostLoaded).post[i].id == event.postId) {
+              post.add(
+                PostEntite(
+                  id: (state as PostLoaded).post[i].id,
+                  author: (state as PostLoaded).post[i].author,
+                  text: (state as PostLoaded).post[i].text,
+                  created_at: (state as PostLoaded).post[i].created_at,
+                  images: (state as PostLoaded).post[i].images,
+                  videos: const [],
+                  numberOfLike: (state as PostLoaded).post[i].numberOfLike,
+                  comments: (state as PostLoaded).post[i].comments,
+                  isILiked: true,
+                  likeIdILike: 1,
+                ),
+              );
+            } else {
+              post.add((state as PostLoaded).post[i]);
+            }
+          }
+          emit(PostLoaded(post: post));
+        }
+      });
     });
   }
 }
--- mobile/lib/features/common/presentation/bloc/post/post_event.dart
+++ mobile/lib/features/common/presentation/bloc/post/post_event.dart
@@ -6,5 +6,15 @@ sealed class PostEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
- 
+
 class GetAllPostsEvent extends PostEvent {}
+
+class LikePostEvent extends PostEvent {
+  final int postId;
+  const LikePostEvent({required this.postId});
+}
+
+class UnLikePostEvent extends PostEvent {
+  final int postId, likeId;
+  const UnLikePostEvent({required this.postId, required this.likeId});
+}
